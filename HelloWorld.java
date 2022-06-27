74c3b12a06ab529c95efa497c2aad21700edd8e2
Jack Li <48315282+Hin97@users.noreply.github.com>
2022-06-27 22:08:32 +1000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args)
   {
-    System.out.println("......***** Make Some Changes *****......");
+    System.out.println("......***** Make Big Changes *****......");
   }
 }
